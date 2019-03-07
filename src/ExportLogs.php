08a68ce1471338d45a7e1bb620d969b6e3d63396
Teddy Aryono <teddy.aryono@gmail.com>
2019-03-08 02:17:03 +0700

    Add disks from Service Provider

--- src/ExportLogs.php
+++ src/ExportLogs.php
@@ -3,6 +3,7 @@
 namespace Compro\LogExporter;
 
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Storage;
 
 class ExportLogs extends Command
 {
@@ -18,7 +19,7 @@ class ExportLogs extends Command
      *
      * @var string
      */
-    protected $description = 'Command description';
+	protected $description = 'Export all "storage/logs/*.log" file to S3, except "laravel.log" file.';
 
     /**
      * Create a new command instance.
@@ -37,6 +38,34 @@ class ExportLogs extends Command
      */
     public function handle()
     {
+	    // User Notification: Export log in progress
 	    $this->line("<fg=red>Export log in progress...</>");
+
+
+	    // Get all local log files, except laravel.log
+	    $localLogDisk = Storage::disk('local_log');
+	    $localLogFiles = collect($localLogDisk->allFiles());
+	    $logFilesToBeExported = $localLogFiles->map(function($item) {
+		    $excludeFile = 'laravel.log';
+		    return $item == $excludeFile ? null : $item;
+	    })
+			->filter();
+
+
+	    // Upload all local log files to S3 bucket
+	    $cloudDisk = Storage::disk('cloud_log');
+
+	    $logFilesToBeExported->each(function ($item, $key) use ($cloudDisk) {
+		    $cloudDisk->put($item, $item);
+	    });
+
+	    $cloudLogFiles = collect($cloudDisk->allFiles());
+
+	    $this->line("\n<fg=red>List files in the bucket...</>");
+	    $cloudLogFiles->dump();
+
+
+	    // User Notification: Export log done
+	    $this->line("\n<fg=red>Export log done...</>");
     }
 }
--- src/LogExporterServiceProvider.php
+++ src/LogExporterServiceProvider.php
@@ -18,6 +18,24 @@ class LogExporterServiceProvider extends ServiceProvider
         		ExportLogs::class,
 	        ]);
         }
+
+
+        // Registering local path of logs
+	    app()->config['filesystems.disks.local_log'] = [
+		    'driver' => 'local',
+		    'root' => storage_path('logs'),
+	    ];
+
+
+	    // Registering cloud path of logs
+	    app()->config['filesystems.disks.cloud_log'] = [
+		    'driver' => 's3',
+		    'key' => env('AWS_LOG_ACCESS_KEY_ID'),
+		    'secret' => env('AWS_LOG_SECRET_ACCESS_KEY'),
+		    'region' => env('AWS_LOG_DEFAULT_REGION'),
+		    'bucket' => env('AWS_LOG_BUCKET'),
+	    ];
+
     }
 
     /**
